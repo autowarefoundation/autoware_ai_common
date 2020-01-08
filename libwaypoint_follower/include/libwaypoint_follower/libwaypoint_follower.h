d2a14112de3da02447bae833c55229875d8060ac
Daisuke Nishimatsu <nishimarudai@gmail.com>
2020-01-08 21:26:55 +0000

    Add libraries for path predictor
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- libwaypoint_follower/include/libwaypoint_follower/libwaypoint_follower.h
+++ libwaypoint_follower/include/libwaypoint_follower/libwaypoint_follower.h
@@ -14,18 +14,29 @@
  * limitations under the License.
  */
 
-#ifndef _LIB_WAYPOINT_FOLLOWER_H_
-#define _LIB_WAYPOINT_FOLLOWER_H_
+#ifndef LIBWAYPOINT_FOLLOWER_LIBWAYPOINT_FOLLOWER_H
+#define LIBWAYPOINT_FOLLOWER_LIBWAYPOINT_FOLLOWER_H
+
+#define EIGEN_MPL2_ONLY
+#include <tf/transform_datatypes.h>
+#include <tf2/utils.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 
 // C++ header
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <utility>
+#include <vector>
 
 // ROS header
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include "autoware_msgs/Lane.h"
+
+constexpr double ERROR = 1e-6;
 
 enum class LaneDirection : int
 {
@@ -96,4 +107,27 @@ int getClosestWaypoint(const autoware_msgs::Lane& current_path, geometry_msgs::P
 bool getLinearEquation(geometry_msgs::Point start, geometry_msgs::Point end, double* a, double* b, double* c);
 double getDistanceBetweenLineAndPoint(geometry_msgs::Point point, double sa, double b, double c);
 double getRelativeAngle(geometry_msgs::Pose waypoint_pose, geometry_msgs::Pose vehicle_pose);
-#endif
+double calcCurvature(const geometry_msgs::Point &target, const geometry_msgs::Pose &curr_pose);
+double calcDistSquared2D(const geometry_msgs::Point &p, const geometry_msgs::Point &q);
+double calcLateralError2D(const geometry_msgs::Point &a_start, const geometry_msgs::Point &a_end,
+                          const geometry_msgs::Point &b);
+double calcRadius(const geometry_msgs::Point &target, const geometry_msgs::Pose &current_pose);
+std::vector<geometry_msgs::Pose> extractPoses(const autoware_msgs::Lane &lane);
+std::vector<geometry_msgs::Pose> extractPoses(const std::vector<autoware_msgs::Waypoint> &wps);
+std::pair<bool, int32_t> findClosestIdxWithDistAngThr(const std::vector<geometry_msgs::Pose> &curr_ps,
+                                                      const geometry_msgs::Pose &curr_pose,
+                                                      double dist_thr = 3.0,
+                                                      double angle_thr = M_PI_2);
+geometry_msgs::Quaternion getQuaternionFromYaw(const double &_yaw);
+bool isDirectionForward(const std::vector<geometry_msgs::Pose> &poses);
+double normalizeEulerAngle(double euler);
+geometry_msgs::Point transformToAbsoluteCoordinate2D(const geometry_msgs::Point &point,
+                                                                      const geometry_msgs::Pose &current_pose);
+geometry_msgs::Point transformToAbsoluteCoordinate3D(const geometry_msgs::Point &point,
+                                                                      const geometry_msgs::Pose &origin);
+geometry_msgs::Point transformToRelativeCoordinate2D(const geometry_msgs::Point &point,
+                                                                      const geometry_msgs::Pose &current_pose);
+geometry_msgs::Point transformToRelativeCoordinate3D(const geometry_msgs::Point &point,
+                                                                      const geometry_msgs::Pose &current_pose);
+
+#endif  // LIBWAYPOINT_FOLLOWER_LIBWAYPOINT_FOLLOWER_H
--- libwaypoint_follower/test/src/test_libwaypoint_follower.cpp
+++ libwaypoint_follower/test/src/test_libwaypoint_follower.cpp
@@ -16,18 +16,25 @@
 
 #include "test_libwaypoint_follower.hpp"
 
-class LibWaypointFollowerTestSuite : public ::testing::Test {
+#include <utility>
+#include <map>
+#include <string>
+#include <vector>
+
+class LibWaypointFollowerTestSuite : public ::testing::Test
+{
 public:
   LibWaypointFollowerTestSuite() {}
   ~LibWaypointFollowerTestSuite() {}
   LibWaypointFollowerTestClass test_obj_;
 
 protected:
-  virtual void SetUp(){}
-  virtual void TearDown(){}
+  virtual void SetUp() {}
+  virtual void TearDown() {}
 };
 
-TEST_F(LibWaypointFollowerTestSuite, calcRelativeCoordinate) {
+TEST_F(LibWaypointFollowerTestSuite, calcRelativeCoordinate)
+{
 //  The member variable x of calcRelativeCoordinate must return following:
 //  - if      target_point is in front of current_pose,  return positive value
 //  - else if target_point is behind from current_pose,  return negative value
@@ -56,7 +63,8 @@ TEST_F(LibWaypointFollowerTestSuite, calcRelativeCoordinate) {
   }
 }
 
-TEST_F(LibWaypointFollowerTestSuite, getLaneDirectionByPosition) {
+TEST_F(LibWaypointFollowerTestSuite, getLaneDirectionByPosition)
+{
 //  getLaneDirectionByPosition must return following:
 //  - if      waypoint[i+1] is in front of waypoint[i],  return Forward
 //  - else if waypoint[i+1] is behind from waypoint[i],  return Backward
@@ -74,7 +82,8 @@ TEST_F(LibWaypointFollowerTestSuite, getLaneDirectionByPosition) {
   }
 }
 
-TEST_F(LibWaypointFollowerTestSuite, getLaneDirectionByVelocity) {
+TEST_F(LibWaypointFollowerTestSuite, getLaneDirectionByVelocity)
+{
 //  getLaneDirectionByVelocity must return following:
 //  - if waypoint[i+1] is in front of waypoint[i],      return Forward
 //  - else if waypoint[i+1] is behind from waypoint[i], return Backward
@@ -90,7 +99,8 @@ TEST_F(LibWaypointFollowerTestSuite, getLaneDirectionByVelocity) {
   }
 }
 
-TEST_F(LibWaypointFollowerTestSuite, getLaneDirection) {
+TEST_F(LibWaypointFollowerTestSuite, getLaneDirection)
+{
 //  getLaneDirection must return following:
 //   Now, Rp means position identification result,
 //    and Rv means velocity identification result.
@@ -116,7 +126,8 @@ TEST_F(LibWaypointFollowerTestSuite, getLaneDirection) {
   }
 }
 
-TEST_F(LibWaypointFollowerTestSuite, inDrivingDirection) {
+TEST_F(LibWaypointFollowerTestSuite, inDrivingDirection)
+{
 //  inDrivingDirection must return following:
 //   Now, Rd means lane direction identification result,
 //    and Rc means relative Coordinate result.
@@ -151,7 +162,8 @@ TEST_F(LibWaypointFollowerTestSuite, inDrivingDirection) {
 }
 
 
-TEST_F(LibWaypointFollowerTestSuite, getClosestWaypoint) {
+TEST_F(LibWaypointFollowerTestSuite, getClosestWaypoint)
+{
 //
 // getClosestWaypoint must return following:
 //   case) current_pose == (0,0,0)
@@ -190,7 +202,302 @@ TEST_F(LibWaypointFollowerTestSuite, getClosestWaypoint) {
   }
 }
 
-int main(int argc, char **argv) {
+TEST_F(LibWaypointFollowerTestSuite, calcCurvature)
+{
+  geometry_msgs::Point target;
+  geometry_msgs::Pose curr_pose;
+
+  target.x = 10.0;
+  target.y = 0.0;
+  ASSERT_NEAR(0.0, calcCurvature(target, curr_pose), ERROR);
+
+  target.x = 10.0;
+  target.y = 10.0;
+  ASSERT_NEAR(0.1, calcCurvature(target, curr_pose), ERROR);
+
+  target.x = 0.0;
+  target.y = 10.0;
+  ASSERT_NEAR(0.2, calcCurvature(target, curr_pose), ERROR);
+}
+
+TEST_F(LibWaypointFollowerTestSuite, calcDistSquared2D)
+{
+  geometry_msgs::Point p;
+  geometry_msgs::Point q;
+
+  // p.x = 0.0, p.y = 0.0, q.x = 0.0, q.y = 0.0, length2=0.0m
+  ASSERT_NEAR(0.000000, calcDistSquared2D(p, q), ERROR);
+
+  // p.x = 0.0, p.y = 0.0, p.z = 10.0, q.x = 0.0, q.y = 0.0, length2=0.0m, z is invalid value
+  p.z = 10.0;
+  ASSERT_NEAR(0.000000, calcDistSquared2D(p, q), ERROR);
+
+  // p.x = 1.0, p.y = 0.0, q.x = 0.0, q.y = 0.0, length2=0.0m
+  p.x = 1.0;
+  p.z = 0.0;
+  ASSERT_NEAR(1.000000, calcDistSquared2D(p, q), ERROR);
+
+  // p.x = 2.0, p.y = 6.0, q.x = 6.0, q.y = 3.0, length2=25.0m
+  p.x = 2.0;
+  p.y = 6.0;
+  q.x = 6.0;
+  q.y = 3.0;
+  ASSERT_NEAR(25.000000, calcDistSquared2D(p, q), ERROR);
+}
+
+TEST_F(LibWaypointFollowerTestSuite, calcLateralError)
+{
+  geometry_msgs::Point line_s;
+  geometry_msgs::Point line_e;
+  geometry_msgs::Point point;
+
+  // target point is on right side of the line
+  line_s.x = 2.0;
+  line_s.y = 4.0;
+
+  line_e.x = 7.0;
+  line_e.y = 3.0;
+
+  point.x = 6.0;
+  point.y = -3.0;
+  ASSERT_NEAR(-6.079600, calcLateralError2D(line_s, line_e, point), ERROR);
+
+  // target point is on left side of the line
+  point.x = 4.0;
+  point.y = 8.0;
+  ASSERT_NEAR(4.314555, calcLateralError2D(line_s, line_e, point), ERROR);
+
+  // the length of line is zero
+  ASSERT_NEAR(0.0, calcLateralError2D(line_s, line_s, point), ERROR);
+}
+
+TEST_F(LibWaypointFollowerTestSuite, calcRadius)
+{
+  geometry_msgs::Point target;
+  geometry_msgs::Pose curr_pose;
+
+  ASSERT_NEAR(1e9, calcRadius(target, curr_pose), ERROR);
+
+  target.x = 10.0;
+  target.y = 0.0;
+  ASSERT_NEAR(1e9, calcRadius(target, curr_pose), ERROR);
+
+  target.x = 10.0;
+  target.y = 10.0;
+  ASSERT_NEAR(10.0, calcRadius(target, curr_pose), ERROR);
+
+  target.x = 0.0;
+  target.y = 10.0;
+  ASSERT_NEAR(5.0, calcRadius(target, curr_pose), ERROR);
+}
+
+TEST_F(LibWaypointFollowerTestSuite, extractPoses_Lane)
+{
+  autoware_msgs::Lane test;
+  test.waypoints.emplace_back();
+  test.waypoints.emplace_back();
+  test.waypoints.emplace_back();
+  test.waypoints.at(0).pose.pose.position.x = 5.0;
+  test.waypoints.at(1).pose.pose.position.x = 100.0;
+  test.waypoints.at(2).pose.pose.position.x = -999.0;
+  std::vector<geometry_msgs::Pose> poses = extractPoses(test);
+  ASSERT_NEAR(5.0, test.waypoints.at(0).pose.pose.position.x, ERROR);
+  ASSERT_NEAR(100.0, test.waypoints.at(1).pose.pose.position.x, ERROR);
+  ASSERT_NEAR(-999.0, test.waypoints.at(2).pose.pose.position.x, ERROR);
+}
+TEST_F(LibWaypointFollowerTestSuite, extractPoses_Waypoint)
+{
+  std::vector<autoware_msgs::Waypoint> test;
+  test.emplace_back();
+  test.emplace_back();
+  test.emplace_back();
+  test.at(0).pose.pose.position.x = 5.0;
+  test.at(1).pose.pose.position.x = 100.0;
+  test.at(2).pose.pose.position.x = -999.0;
+  std::vector<geometry_msgs::Pose> poses = extractPoses(test);
+  ASSERT_NEAR(5.0, test.at(0).pose.pose.position.x, ERROR);
+  ASSERT_NEAR(100.0, test.at(1).pose.pose.position.x, ERROR);
+  ASSERT_NEAR(-999.0, test.at(2).pose.pose.position.x, ERROR);
+}
+
+TEST_F(LibWaypointFollowerTestSuite, findClosestIdxWithDistAngThr)
+{
+  std::vector<geometry_msgs::Pose> curr_ps;
+  geometry_msgs::Pose curr_pose;
+
+  std::pair<bool, int32_t> ans;
+
+  ans = findClosestIdxWithDistAngThr(curr_ps, curr_pose);
+  ASSERT_EQ(false, ans.first);
+  ASSERT_EQ(-1, ans.second);
+
+  tf2::Quaternion tf2_q;
+  tf2_q.setRPY(0, 0, 0);
+  tf2_q.normalize();
+  for (uint32_t i = 0; i < 10; i++)
+  {
+    geometry_msgs::Pose tmp_pose;
+    tmp_pose.position.x = static_cast<double>(i) * 1.0;
+    tmp_pose.orientation = tf2::toMsg(tf2_q);
+    curr_ps.push_back(tmp_pose);
+  }
+
+  curr_pose.position.x = 0.0;
+  curr_pose.orientation = tf2::toMsg(tf2_q);
+  ans = findClosestIdxWithDistAngThr(curr_ps, curr_pose);
+  ASSERT_EQ(true, ans.first);
+  ASSERT_EQ(0, ans.second);
+
+  tf2_q.setRPY(0, 0, 0.175);  // yaw = 10deg
+  tf2_q.normalize();
+  curr_pose.position.x = 3.0;
+  curr_pose.orientation = tf2::toMsg(tf2_q);
+  ans = findClosestIdxWithDistAngThr(curr_ps, curr_pose);
+  ASSERT_EQ(true, ans.first);
+  ASSERT_EQ(3, ans.second);
+}
+
+TEST_F(LibWaypointFollowerTestSuite, isDirectionForward)
+{
+  std::vector<geometry_msgs::Pose> poses;
+  poses.reserve(2);
+  geometry_msgs::Pose p0;
+  geometry_msgs::Pose p1;
+  geometry_msgs::Pose p2;
+
+  // p1 and p2 is same pose
+  poses.push_back(p0);
+  poses.push_back(p1);
+  poses.push_back(p2);
+  ASSERT_EQ(false, isDirectionForward(poses));
+
+  // p2.position.x = 3.0 p1 is origin position
+  poses.clear();
+  poses.push_back(p0);
+  poses.push_back(p1);
+  p2.position.x = 3.0;
+  poses.push_back(p2);
+  ASSERT_EQ(true, isDirectionForward(poses));
+
+  // p2.position.x = -3.0 p1 is origin position
+  poses.clear();
+  poses.push_back(p0);
+  poses.push_back(p1);
+  p2.position.x = -3.0;
+  poses.push_back(p2);
+  ASSERT_EQ(false, isDirectionForward(poses));
+}
+
+TEST_F(LibWaypointFollowerTestSuite, normalizeEulerAngle)
+{
+  ASSERT_DOUBLE_EQ(M_PI, normalizeEulerAngle(3 * M_PI));
+  ASSERT_DOUBLE_EQ(-M_PI, normalizeEulerAngle(-3 * M_PI));
+  ASSERT_DOUBLE_EQ(-M_PI + 0.1, normalizeEulerAngle(M_PI + 0.1));
+  ASSERT_DOUBLE_EQ(M_PI - 0.2, normalizeEulerAngle(-M_PI - 0.2));
+}
+
+TEST_F(LibWaypointFollowerTestSuite, transformToAbsoluteCoordinate2D)
+{
+  geometry_msgs::Point point;
+  geometry_msgs::Pose origin;
+  geometry_msgs::Point res;
+
+  // no translation and rotation
+  res = transformToAbsoluteCoordinate2D(point, origin);
+  ASSERT_NEAR(0.0, res.x, ERROR);
+  ASSERT_NEAR(0.0, res.y, ERROR);
+  ASSERT_NEAR(0.0, res.z, ERROR);
+
+  // only translation
+  point.x = 2.0;
+  point.y = 2.0;
+  origin.position.x = 5.0;
+  origin.position.y = 3.0;
+
+  res = transformToAbsoluteCoordinate2D(point, origin);
+  ASSERT_NEAR(7.0, res.x, ERROR);
+  ASSERT_NEAR(5.0, res.y, ERROR);
+  ASSERT_NEAR(0.0, res.z, ERROR);
+
+  // translation and rotation 90 deg
+  tf2::Quaternion tf_q;
+  point.x = 3.0;
+  point.y = 2.0;
+  origin.position.x = 4.0;
+  origin.position.y = 3.0;
+  tf_q.setRPY(0.0, 0.0, 90 * M_PI / 180);
+  origin.orientation = tf2::toMsg(tf_q);
+
+  res = transformToAbsoluteCoordinate2D(point, origin);
+  ASSERT_NEAR(2.0, res.x, ERROR);
+  ASSERT_NEAR(6.0, res.y, ERROR);
+  ASSERT_NEAR(0.0, res.z, ERROR);
+
+  // translation and rotation -90 deg
+  point.x = 2.0;
+  point.y = -3.0;
+  origin.position.x = 5.0;
+  origin.position.y = -4.0;
+  tf_q.setRPY(0.0, 0.0, -90 * M_PI / 180);
+  origin.orientation = tf2::toMsg(tf_q);
+
+  res = transformToAbsoluteCoordinate2D(point, origin);
+  ASSERT_NEAR(2.0, res.x, ERROR);
+  ASSERT_NEAR(-6.0, res.y, ERROR);
+  ASSERT_NEAR(0.0, res.z, ERROR);
+}
+
+TEST_F(LibWaypointFollowerTestSuite, transformToRelativeCoordinate2D)
+{
+  geometry_msgs::Point point;
+  geometry_msgs::Pose origin;
+  geometry_msgs::Point res;
+
+  // no translation and rotation
+  res = transformToRelativeCoordinate2D(point, origin);
+  ASSERT_NEAR(0.0, res.x, ERROR);
+  ASSERT_NEAR(0.0, res.y, ERROR);
+  ASSERT_NEAR(0.0, res.z, ERROR);
+
+  // only translation
+  point.x = 7.0;
+  point.y = 5.0;
+  origin.position.x = 5.0;
+  origin.position.y = 3.0;
+  res = transformToRelativeCoordinate2D(point, origin);
+  ASSERT_NEAR(2.0, res.x, ERROR);
+  ASSERT_NEAR(2.0, res.y, ERROR);
+  ASSERT_NEAR(0.0, res.z, ERROR);
+
+  // translation and rotation 90 deg
+  tf2::Quaternion tf_q;
+
+  point.x = 2.0;
+  point.y = 6.0;
+  origin.position.x = 4.0;
+  origin.position.y = 3.0;
+  tf_q.setRPY(0.0, 0.0, 90 * M_PI / 180);
+  origin.orientation = tf2::toMsg(tf_q);
+  res = transformToRelativeCoordinate2D(point, origin);
+  ASSERT_NEAR(3.0, res.x, ERROR);
+  ASSERT_NEAR(2.0, res.y, ERROR);
+  ASSERT_NEAR(0.0, res.z, ERROR);
+
+  // translation and rotation -90 deg
+  point.x = 2.0;
+  point.y = -6.0;
+  origin.position.x = 5.0;
+  origin.position.y = -4.0;
+  tf_q.setRPY(0.0, 0.0, -90 * M_PI / 180);
+  origin.orientation = tf2::toMsg(tf_q);
+  res = transformToRelativeCoordinate2D(point, origin);
+  ASSERT_NEAR(2.0, res.x, ERROR);
+  ASSERT_NEAR(-3.0, res.y, ERROR);
+  ASSERT_NEAR(0.0, res.z, ERROR);
+}
+
+int main(int argc, char **argv)
+{
   testing::InitGoogleTest(&argc, argv);
   ros::init(argc, argv, "LibWaypointFollowerTestNode");
   ros::NodeHandle n;
